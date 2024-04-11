0a94c38ac5c74914f10dfff21676a0171f87bb71
Manh <xuanmanhdieu@gmail.com>
2024-04-11 22:52:11 +0700

    lecture 5

---
+++ lecture-5/lecture-5-chat-api/src/main/java/com/engineerpro/rest/example/controller/BaseController.java
@@ -0,0 +1,17 @@
+package com.engineerpro.rest.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.engineerpro.rest.example.model.App;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public abstract class BaseController {
+
+  @Autowired
+  protected HttpServletRequest request;
+
+  protected App getAuthenticatedApp() {
+    return (App) request.getAttribute("authenticatedApp");
+  }
+}
